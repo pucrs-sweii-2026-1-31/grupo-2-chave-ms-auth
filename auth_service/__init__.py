89a0f401a759aaa77c8ae6a0a605532e10653a2f
enrico-zini <enricoassmannzini@gmail.com>
2026-05-11 20:43:22 -0300

    feat: flask CORS

--- auth_service/__init__.py
+++ auth_service/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from .config import Config
 from .services.db import db
 from .routes import auth as auth_blueprint
@@ -8,6 +9,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    CORS(app)
     db.init_app(app)
     app.register_blueprint(auth_blueprint)
 
